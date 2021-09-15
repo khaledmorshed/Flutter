7d5c3c0b2fe4b7671f3f5445f0841f80c37f8e2d
S.M Khaled Morshed <44377065+khaledmorshed@users.noreply.github.com>
2021-09-15 11:31:36 +0600

    mainAxis

---
+++ Day_7/mainAxis.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myrow_colApp());
+}
+
+class Myrow_colApp extends StatelessWidget {
+  const Myrow_colApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Row Col App"),
+        ),
+        body: Container(
+          color: Colors.greenAccent,
+          //width: double.infinity,
+         // alignment: Alignment.center,
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+              children: [Padding(padding: EdgeInsets.all(8.0), child: ElevatedButton(
+                onPressed: (){}, child: Padding(padding: EdgeInsets.all(8.0), child: Text("Button1"),
+              )
+              ),
+              ),
+             Padding(padding: EdgeInsets.all(8.0), child: ElevatedButton(
+                onPressed: (){}
+              , child: Padding(padding: EdgeInsets.all(100.0), child: Text("Button2"),
+              )
+              ),
+              ),
+              /*Padding(padding: EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 5.0), child: ElevatedButton(
+                onPressed: (){}
+              , child: Padding(padding: EdgeInsets.all(10.0), child: Text("Button3"),
+              )
+              ),
+              ),
+              Padding(padding: EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 5.0), child: ElevatedButton(
+                onPressed: (){}
+              , child: Padding(padding: EdgeInsets.all(5.0), child: Text("Button4"),
+              )
+              ),
+              ),*/
+              ],
+            ),
+        ),
+
+        ),   
+    );
+  }
+}
