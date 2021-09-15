a90927e7e4e6eff86f8c53fa9696c62089551879
S.M Khaled Morshed <44377065+khaledmorshed@users.noreply.github.com>
2021-09-15 10:06:23 +0600

    Update row_col.dart

--- Day_7/row_col.dart
+++ Day_7/row_col.dart
@@ -1,1 +1,29 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(Myrow_colApp());
+}
+
+class Myrow_colApp extends StatelessWidget {
+  const Myrow_colApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Row Col App"),
+        ),
+        body: Column(
+            children: [Text("Hello world", style: TextStyle(fontSize: 40.0, color: Colors.deepOrange, fontWeight: FontWeight.w900)
+            ),
+             Text("Hello world", style: TextStyle(fontSize: 40.0, color: Colors.deepOrange, fontWeight: FontWeight.w900
+             )
+             )
+            ],
+          ),
+
+        ),   
+    );
+  }
+}
