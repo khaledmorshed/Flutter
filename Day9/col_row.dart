dcbf8627bf9168706163e7dc43e91753843f959f
S.M Khaled Morshed <44377065+khaledmorshed@users.noreply.github.com>
2021-09-20 10:35:18 +0600

    Create col_row.dart

---
+++ Day9/col_row.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MySplashScreenOne());
+}
+
+class MySplashScreenOne extends StatelessWidget {
+  const MySplashScreenOne({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("splash Screen"),
+        ),
+        body: Column(
+          children: [
+            ElevatedButton
+          (onPressed: (){}
+          , child: Icon(Icons.access_alarm_sharp),
+          style: ElevatedButton.styleFrom(
+            primary: Colors.cyanAccent
+          ),
+          ),
+          ElevatedButton
+          (onPressed: (){}
+          , child: Icon(Icons.access_alarm_sharp)
+          ),
+          ElevatedButton
+          (onPressed: (){}
+          , child: Icon(Icons.access_alarm_sharp)
+          ),
+          
+          ],
+        )
+      ),
+    );
+  }
+}
